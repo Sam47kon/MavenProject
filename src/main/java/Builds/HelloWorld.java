ab2ff9951d325dccabb67bf287ad072ffb15a555
Sam47kon <Sam47kon@gmail.com>
2019-03-05 23:35:50 +0300

    123

---
+++ src/main/java/Builds/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Программы для вывода
+ * @author Sam47kon
+ * @version 1.0
+ */
+package Builds;
+
+public class HelloWorld {
+    /**
+     * Программа
+     * @param args команды линейных аргументов
+     */
+    public static void main(String[] args) {
+
+        System.out.println("HalloWorld");
+
+
+
+
+        //System.exit(-1); завершает программу
+    }
+}
---
+++ src/main/java/Builds/Task1.java
@@ -0,0 +1,12 @@
+/*
+package Builds;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(Task1.getPi());
+    }
+    public static String getPi(){
+        return "3.14";
+    }
+}
+*/
