a671702e77e9c6ff07e3f86d35b1d14aa823dcd2
Ilya Bulavin <Sam47kon@gmail.com>
2019-03-07 21:00:56 +0300

    подключил тест вроде как

--- src/Test/Java/GrandTasks/Task1Test.java
+++ src/Test/Java/GrandTasks/Task1Test.java
@@ -1,12 +1,15 @@
 package GrandTasks;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class Task1Test extends Task1 {
     @Test
-    public void Test(){
-        // assertEquals(2,2+ что делать???
+    public void Test() {
+        int a = 5;
+        assertEquals(Square(a), a*a);
+
     }
 }
--- src/main/java/GrandTasks/Task1.java
+++ src/main/java/GrandTasks/Task1.java
@@ -7,13 +7,13 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in); //подключение ввода с консоли
         System.out.println("Enter number");
-        int number = reader.nextInt(); // ввод с консоли в новую переменную
+        int a = reader.nextInt(); // ввод с консоли в новую переменную
 
-        System.out.println("Your square: " + Square(number));
-        System.out.println("Yor number: " + number);
+        System.out.println("Your square: " + Square(a));
+        System.out.println("Yor number: " + a);
     }
 
-    private static int Square(int number) { //возведение в квадрат, без записи в переменную
+    public static int Square(int number) { //возведение в квадрат, без записи в переменную
         return number * number;
     }
 }
