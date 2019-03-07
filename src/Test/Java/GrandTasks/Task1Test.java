48e978144bd6bbeb1f69d0208df62560881b1e5c
Ilya Bulavin <Sam47kon@gmail.com>
2019-03-07 19:59:41 +0300

    подключил тест

--- src/Test/Java/GrandTasks/Task1Test.java
+++ src/Test/Java/GrandTasks/Task1Test.java
@@ -1,7 +1,12 @@
 package GrandTasks;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class Task1Test {
 
+class Task1Test extends Task1 {
+    @Test
+    public void Test(){
+        // assertEquals(2,2+ что делать???
+    }
 }
