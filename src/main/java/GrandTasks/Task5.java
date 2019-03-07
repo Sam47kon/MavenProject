5574042740d40c9b0a88ecf29828e11b916b9a17
Ilya Bulavin <Sam47kon@gmail.com>
2019-03-08 01:25:10 +0300

    5 еще не сделал

--- src/main/java/GrandTasks/Task5.java
+++ src/main/java/GrandTasks/Task5.java
@@ -2,13 +2,28 @@ package GrandTasks;
 
 import java.util.Scanner;
 
-//
+// еще не сделал
 public class Task5 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Enter 1 number");
+        System.out.println("Введите первое число: ");
         int a = reader.nextInt();
-        System.out.println("Enter 2 number");
+        System.out.println("Введите второе число: ");
         int b = reader.nextInt();
+        int c = 0;
+        System.out.println("Сложить '+', вычесть '-', умножить '*', поделить '/' ");
+        String sign = reader.next();
+      //  while (sign == "p" || sign == "-" || sign == "*" || sign == "/") {
+            if (sign == "p") {
+                c = a + b;
+            } else if (sign == "-") {
+                c = a - b;
+            } else if (sign == "*") {
+                c = a * b;
+            } else if (sign == "/") {
+                c = a / b;
+         //   } else System.out.println("Попробуй еще раз.");
+        }
+        System.out.println(c + sign);
     }
 }
