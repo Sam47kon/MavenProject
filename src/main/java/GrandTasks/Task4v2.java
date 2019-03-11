71cb9edd2da0d4ab06e946f18c2b9bd9fae7e9a3
Sam47kon <Sam47kon@gmail.com>
2019-03-11 21:26:12 +0300

    corrected Task4v2

--- src/main/java/GrandTasks/Task4v2.java
+++ src/main/java/GrandTasks/Task4v2.java
@@ -5,34 +5,39 @@ import java.util.Scanner;
 // Среднее арифметическое введеных нескольких чисел
 public class Task4v2 {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
         System.out.println("Введите колличество чисел для вывода среднего значения: ");
-        int numbers[] = new Task4v2().getArray(reader); // создаю массив, используя метод ArrayLength
-        double average = new Task4v2().getAverage(numbers);
-        System.out.println("Среднее арифметическое чисел: " + average);
-
+        while (true) {
+            int[] numbers = new Task4v2().creatingArrayWithGivenNumbersAndLength();
+            if (numbers.length != 0) {
+                double average = new Task4v2().getAverage(numbers);
+                System.out.println("Среднее арифметическое чисел: " + average);
+                break;
+            }
+            System.out.println("Нельзя посчитать среднее арфиметическое без чисел! Введите количество (оно не может быть 0)");
+        }
     }
 
-    // метод создания массива с указанием чисел и длинны
-    private int[] getArray(Scanner reader) {
+    // метод создания массива с указанием чисел и длинны, creating an array with given numbers and its length
+    private int[] creatingArrayWithGivenNumbersAndLength() {
+        Scanner reader = new Scanner(System.in);
         int quantity = reader.nextInt();
-        int[] numbers = new int[quantity];
-        for (int i = 0; i < numbers.length; i++) {
+        int[] values = new int[quantity];
+        for (int i = 0; i < values.length; i++) {
             System.out.println("Enter " + ++i + " number");
             i--;
-            numbers[i] = reader.nextInt();
+            values[i] = reader.nextInt();
         }
-        return numbers;
+        return values;
     }
 
-    // метод
-    public double getAverage(int a[]) {
+    // метод нахождение среднего
+    public double getAverage(int[] mass) {
         double average;
         double sum = 0;
-        for (int i = 0; i < a.length; i++) {
-            sum += a[i];
+        for (int i : mass) {
+            sum += i;
         }
-        average = sum / a.length;
+        average = sum / mass.length;
         return average;
     }
 }
