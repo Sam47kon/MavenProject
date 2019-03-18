8e116daf0c3ed202013f3ab6b781ff4cfb2a4d04
Sam47kon <Sam47kon@gmail.com>
2019-03-19 00:44:04 +0300

    completed Task14 & я понял что все время делал что то не так... я постоянно пытался вернуть какое то числовое значение и чтобы красиво вывоилось, нооооо веееедь моооожнооо вернуть все что угодно, например ТЕЕЕЕЕКККСТТ

---
+++ src/main/java/GrandTasks/Week3/Task14.java
@@ -0,0 +1,31 @@
+package GrandTasks.Week3;
+
+import GrandTasks.Week2.Task5;
+
+//Вводишь 10 чисел, и число, которое надо найти в этом списке. Программа возвращает индекс, или -1 если введенное число не присутствует
+public class Task14 {
+
+    public static void main(String[] args) {
+        double[] inputNumbers = new double[5];
+        for (int i = 0; i < inputNumbers.length; ) {
+            inputNumbers[i] = new Task5().getNumber("Insert the " + ++i + " number");
+        }
+        double desiredNumber = new Task5().getNumber("Введите любое число, и я вам скажу есть ли оно среди тех чисел");
+        boolean doesItExist = new Task14().findOutThatItExists(inputNumbers, desiredNumber);
+        if (doesItExist) {
+            System.out.println("1");
+        } else {
+            System.out.println("-1");   // решение, если надо вывести -1
+        }
+    }
+
+    public boolean findOutThatItExists(double[] inputNumbers, double desiredNumber) {
+        for (int i = 0; i < inputNumbers.length; i++) {
+            if (inputNumbers[i] == desiredNumber) {
+                System.out.println("Ваше число " + desiredNumber + " стоит на " + (i + 1) + " месте");
+                return true;
+            }
+        }
+        return false;
+    }
+}
