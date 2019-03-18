0f1f2e7229e569aa338fcaad12bc8c103ffb687b
Sam47kon <Sam47kon@gmail.com>
2019-03-18 21:47:28 +0300

    completed Task13 & Test13

---
+++ src/Test/Java/GrandTasks/Week2/Task13extraV2Test.java
@@ -0,0 +1,18 @@
+package GrandTasks.Week2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Task13extraV2Test {
+
+
+    @Test
+    void selectionSortTest() {
+        double[] arr1 = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        double[] sortArr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Assertions.assertArrayEquals(sortArr1, new Task13extraV2().selectionSort(arr1));
+        double[] arr2 = {99999, 999.7, 7765, 23754, -3.7, -2, 0, 1, 5};
+        double[] sortArr2 = {-3.7, -2, 0, 1, 5, 999.7, 7765, 23754, 99999};
+        Assertions.assertArrayEquals(sortArr2, new Task13extraV2().selectionSort(arr2));
+    }
+}
---
+++ src/main/java/GrandTasks/Week2/Task13extraV2.java
@@ -0,0 +1,39 @@
+package GrandTasks.Week2;
+
+// Вводишь 10 чисел, программа возвращает их в отсортированном порядке
+public class Task13extraV2 {
+    public static void main(String[] args) {
+        double[] inputNumbers = new double[10];
+        for (int i = 0; i < inputNumbers.length; ) {
+            inputNumbers[i] = new Task5().getNumber("Введите " + ++i + " число");
+        }
+        double[] sortNumbers = new Task13extraV2().selectionSort(inputNumbers);    // 2 вариант сортировки
+        for (int i = 0; i < sortNumbers.length; ) {
+            System.out.print(sortNumbers[i++] + "  ");
+        }
+    }
+
+    public double[] selectionSort(double[] arr) {       // нашел в инете, разобрал, понял
+        // По очереди будем просматривать все подмножества элементов массива (0 - последний, 1-последний, 2-последний,...)
+        for (int i = 0; i < arr.length; i++) {
+            // Предполагаем, что первый элемент (в каждом подмножестве элементов) является минимальным
+            double min = arr[i];
+            int min_i = i;
+            // В оставшейся части подмножества ищем элемент, который меньше предположенного минимума
+            for (int j = i + 1; j < arr.length; j++) {
+                // Если находим, запоминаем его индекс
+                if (arr[j] < min) {
+                    min = arr[j];
+                    min_i = j;
+                }
+            }
+            // Если нашелся элемент, меньший, чем на текущей позиции, меняем их местами
+            if (i != min_i) {
+                double tmp = arr[i];
+                arr[i] = arr[min_i];
+                arr[min_i] = tmp;
+            }
+        }
+        return arr;
+    }
+}
