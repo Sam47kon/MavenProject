b212910c07b6126d13327ebd3878a521173b96b7
Sam47kon <Sam47kon@gmail.com>
2019-03-19 21:48:46 +0300

    I figured out how it works BigInteger

--- src/Test/Java/GrandTasks/Week2/Task7Test.java
+++ src/Test/Java/GrandTasks/Week2/Task7Test.java
@@ -1,6 +1,9 @@
 package GrandTasks.Week2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
 
 class Task7Test {
 
@@ -8,6 +11,6 @@ class Task7Test {
     @Test
     void getFactorialTest() { // TODO как протестить?
         int number = 7;
-        // Assertions.assertEquals(5040, new Task7().getFactorial(number));
+        Assertions.assertEquals(BigInteger.valueOf(5040), new Task7().getFactorial(number));
     }
 }
