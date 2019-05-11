89454e2ab6683e431f040a168c9a1ea48a230dd0
Sam47kon <Sam47kon@gmail.com>
2019-05-11 03:22:27 +0300

    work on CustomHashMap, synchronize

--- src/main/java/GrandTasks/Week9_10/CustomHashMap.java
+++ src/main/java/GrandTasks/Week9_10/CustomHashMap.java
@@ -3,47 +3,111 @@ package GrandTasks.Week9_10;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class CustomHashMap<K, V> implements Map<K, V> {
-    private int size;
-    private MyEntry<K, V>[] table;
+    private MyNode<K, V>[] hashTable;
     private Set<Map.Entry<K, V>> entrySet;
+    private int size;
+    private double threshold;   // порог размера
+
+    public CustomHashMap() {
+        hashTable = new MyNode[16];
+        threshold = hashTable.length * 0.75;
+    }
+
+
+    /**
+     * Для вставки в нужную ячейку массива hashTable по переданному ключу
+     *
+     * @return индекс, куда положить пару
+     */
+    private int hash(final K key) {
+        int hash = 17 * 37 + key.hashCode();
+        return hash % hashTable.length;
+    }
+
+    /**
+     * Для вставки в нужную ячейку массива hashTable по переданному node
+     *
+     * @return индекс, куда положить пару
+     */
+    private int hash(MyNode<K, V> node) {
+        return node.hashCode() % hashTable.length;
+    }
+
+    private void expandTable() {
+
+    }
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
+    /**
+     * Содержит ли данная коллекция объект с ключем key
+     *
+     * @return true, если есть такой ключ
+     */
     @Override
     public boolean containsKey(Object key) {
         return false;
     }
 
+    /**
+     * Содержит ли данная коллекция объект value
+     *
+     * @return true, если имеется такой объект
+     */
     @Override
     public boolean containsValue(Object value) {
         return false;
     }
 
+    /**
+     * Возвращает объект коллецкии по ключу key, если таковой есть
+     */
     @Override
     public V get(Object key) {
         return null;
     }
 
+    /**
+     * Вставить пару ключ key и значение value в коллекцию CustomHashMap
+     */
     @Nullable
     @Override
     public V put(K key, V value) {
+        if (size + 1 >= threshold) {
+            threshold *= 2;
+        }
+
+        MyNode<K, V> newNode = new MyNode<>(key, value, null);
+//        int index = hash(newNode);    // без разницы это или:
+        int index = newNode.hash();
+        if (hashTable[index] == null) {
+            hashTable[index] = newNode;
+            return null;
+        }
+
+
+        hashTable[index] = newNode;
         return null;
     }
 
+    /**
+     * Удаляет объект коллекции по ключу key, если таковой имеется
+     *
+     * @return удаленный объект
+     */
     @Override
     public V remove(Object key) {
         return null;
@@ -77,54 +141,76 @@ public class CustomHashMap<K, V> implements Map<K, V> {
         return null;
     }
 
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+
+//    @Override
+//    public int hashCode() {
+//        return super.hashCode();
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        return super.equals(obj);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return super.toString();
+//    }
 
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 
-    static class MyEntry<K, V> implements Map.Entry<K, V> {
-        final int hash;
-        final K key;
-        V value;
-        MyEntry<K, V> next;
+    class MyNode<k, v> implements Map.Entry<k, v> {
+        private MyNode<k, v> next;
+        private int hash;
+        final k key;
+        v value;
 
-        public MyEntry(int hash, K key, V value, MyEntry<K, V> next) {
-            this.hash = hash;
+        private MyNode(k key, v value, MyNode<k, v> next) {
             this.key = key;
             this.value = value;
             this.next = next;
         }
 
+        private int hash() {
+            return hashCode() % hashTable.length;
+        }
+
         @Override
-        public K getKey() {
+        public k getKey() {
             return key;
         }
 
         @Override
-        public V getValue() {
+        public v getValue() {
             return value;
         }
 
         @Override
-        public V setValue(V newValue) { // возвращает старое значение, устанавливает новое
-            V oldValue = value;
+        public v setValue(v newValue) { // возвращает старое значение, устанавливает новое
+            v oldValue = value;
             value = newValue;
             return oldValue;
         }
 
         @Override
         public final int hashCode() {
-            int result = 17 * 37 + key.hashCode();
-            result = result * 37 + value.hashCode();
-            return result;
+            hash = 17 * 37 + key.hashCode();
+            hash = hash * 37 + value.hashCode();
+            return hash;
         }
 
+        @SuppressWarnings("unchecked")
         @Override
         public boolean equals(Object obj) {
             if (obj == this) {
                 return true;
             }
             if (obj instanceof Map.Entry) {
-                return Objects.equals(value, ((MyEntry) obj).getValue()) && Objects.equals(key, ((MyEntry) obj).getKey());
+                MyNode<k, v> node = (MyNode) obj; // unchecked
+                return Objects.equals(value, node.getValue()) && Objects.equals(key, node.getKey());
             }
             return false;
         }
--- src/main/java/GrandTasks/Week9_10/Work.java
+++ src/main/java/GrandTasks/Week9_10/Work.java
@@ -5,8 +5,22 @@ import java.util.HashMap;
 public class Work {
     public static void main(String[] args) {
         HashMap<Integer, String> hashMap = new HashMap<>();
-        hashMap.put(1,"Ilya");
-        System.out.println("hashMap.isEmpty() = " + hashMap.isEmpty());
+        hashMap.put(1, "Ilya");
+        hashMap.put(2, "Alina");
+
         System.out.println("hashMap.toString() = " + hashMap.toString());
+        System.out.println("hashMap.isEmpty() = " + hashMap.isEmpty());
+
+        System.out.println("hashMap.containsKey(1) = " + hashMap.containsKey(1));
+        System.out.println("hashMap.containsKey(2) = " + hashMap.containsKey(2));
+        System.out.println("hashMap.containsKey(3) = " + hashMap.containsKey(3));
+
+        System.out.println("hashMap.containsValue(\"Ilya\" = )" + hashMap.containsValue("Ilya"));
+        System.out.println("hashMap.containsValue(\"Alina\" = )" + hashMap.containsValue("Alina"));
+        System.out.println("hashMap.containsValue(\"nothing\") = " + hashMap.containsValue("nothing"));
+
+        System.out.println("hashMap.size() = " + hashMap.size());
+        System.out.println("hashMap.remove(1) = " + hashMap.remove(1));
+        System.out.println("hashMap.remove(3) = " + hashMap.remove(3));
     }
 }
