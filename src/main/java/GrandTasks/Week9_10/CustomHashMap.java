5a3577b86cf1ea22de0470a79d2b70bd386d41e8
Ilya Bulavin <Sam47kon@gmail.com>
2019-05-12 17:38:40 +0300

    synchronize

--- src/main/java/GrandTasks/Week9_10/CustomHashMap.java
+++ src/main/java/GrandTasks/Week9_10/CustomHashMap.java
@@ -6,8 +6,8 @@ import org.jetbrains.annotations.Nullable;
 import java.util.*;
 
 public class CustomHashMap<K, V> implements Map<K, V> {
+    //    private Set<Map.Entry<K, V>> entrySet;
     private MyNode<K, V>[] hashTable;
-    private Set<Map.Entry<K, V>> entrySet;
     private int size;
     private double threshold;   // порог размера
 
@@ -77,30 +77,78 @@ public class CustomHashMap<K, V> implements Map<K, V> {
      */
     @Override
     public V get(Object key) {
+
         return null;
     }
 
     /**
      * Вставить пару ключ key и значение value в коллекцию CustomHashMap
+     *
+     * @return oldValue, если значение перезаписанно, иначе null
      */
     @Nullable
     @Override
     public V put(K key, V value) {
         if (size + 1 >= threshold) {
             threshold *= 2;
+            increaseTable();
         }
 
-        MyNode<K, V> newNode = new MyNode<>(key, value, null);
+        MyNode<K, V> newNode = new MyNode<>(key, value);
 //        int index = hash(newNode);    // без разницы это или:
         int index = newNode.hash();
+
         if (hashTable[index] == null) {
-            hashTable[index] = newNode;
+            hashTable[index] = new MyNode<>(null, null);
+            hashTable[index].getNodes().add(newNode);
+            size++;
             return null;
         }
 
+        List<MyNode<K, V>> nodes = hashTable[index].getNodes();
 
-        hashTable[index] = newNode;
+        for (MyNode<K, V> node : nodes) {
+            if (keyExist(node, newNode)) {
+                return newValueToExistingKey(node, value);
+            }
+            if (itCollision(node, newNode)) {
+                collision(newNode, nodes);
+                return null;
+            }
+        }
         return null;
+    }
+
+    private void increaseTable() {
+        MyNode<K, V>[] oldTable = hashTable;
+        hashTable = new MyNode[oldTable.length * 2];
+        size = 0;
+        for (MyNode<K, V> nodes : oldTable) {
+            if (nodes != null) {
+                for (MyNode<K, V> node : nodes.getNodes()) {
+                    put(node.key, node.value);
+                }
+            }
+        }
+    }
+
+    private boolean keyExist(MyNode<K, V> node, MyNode<K, V> newNode) {
+        return Objects.equals(node.key, newNode.key) && !Objects.equals(node.value, newNode.value);
+    }
+
+    private V newValueToExistingKey(MyNode<K, V> node, V value) {
+        return node.setValue(value);
+    }
+
+    private boolean itCollision(MyNode<K, V> node, MyNode<K, V> newNode) {
+        return node.hashCode() == newNode.hashCode() &&
+                !Objects.equals(node.key, newNode.key) &&
+                !Objects.equals(node.value, newNode.value);
+    }
+
+    private void collision(MyNode<K, V> newNode, List<MyNode<K, V>> nodes) {
+        nodes.add(newNode);
+        size++;
     }
 
     /**
@@ -141,7 +189,29 @@ public class CustomHashMap<K, V> implements Map<K, V> {
         return null;
     }
 
-//----------------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public String toString() {
+        StringBuilder st = new StringBuilder();
+        st.append("{");
+        if (size == 0) {
+            return st.append("}").toString();
+        }
+        for (MyNode<K, V> node : hashTable) {
+            try {
+                List<MyNode<K, V>> nodes = node.getNodes();
+                if (nodes != null) {
+                    st.append(nodes.toString());
+                }
+            } catch (NullPointerException e) {
+                continue;
+            }
+
+        }
+        return st.append("}").toString();
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 
 //    @Override
@@ -163,15 +233,20 @@ public class CustomHashMap<K, V> implements Map<K, V> {
 //----------------------------------------------------------------------------------------------------------------------
 
     class MyNode<k, v> implements Map.Entry<k, v> {
-        private MyNode<k, v> next;
-        private int hash;
+        //        MyNode<k, v> next;
+        List<MyNode<k, v>> nodes;
         final k key;
+        int hash;
         v value;
 
-        private MyNode(k key, v value, MyNode<k, v> next) {
+        private MyNode(k key, v value) {
             this.key = key;
             this.value = value;
-            this.next = next;
+            nodes = new LinkedList<>();
+        }
+
+        private List<MyNode<k, v>> getNodes() {
+            return nodes;
         }
 
         private int hash() {
@@ -198,7 +273,7 @@ public class CustomHashMap<K, V> implements Map<K, V> {
         @Override
         public final int hashCode() {
             hash = 17 * 37 + key.hashCode();
-            hash = hash * 37 + value.hashCode();
+//            hash = hash * 37 + value.hashCode();
             return hash;
         }
 
@@ -209,7 +284,7 @@ public class CustomHashMap<K, V> implements Map<K, V> {
                 return true;
             }
             if (obj instanceof Map.Entry) {
-                MyNode<k, v> node = (MyNode) obj; // unchecked
+                MyNode<?, ?> node = (MyNode<?, ?>) obj; // unchecked
                 return Objects.equals(value, node.getValue()) && Objects.equals(key, node.getKey());
             }
             return false;
--- src/main/java/GrandTasks/Week9_10/Work.java
+++ src/main/java/GrandTasks/Week9_10/Work.java
@@ -9,18 +9,34 @@ public class Work {
         hashMap.put(2, "Alina");
 
         System.out.println("hashMap.toString() = " + hashMap.toString());
-        System.out.println("hashMap.isEmpty() = " + hashMap.isEmpty());
+//        System.out.println("hashMap.isEmpty() = " + hashMap.isEmpty());
+//
+//        System.out.println("hashMap.containsKey(1) = " + hashMap.containsKey(1));
+//        System.out.println("hashMap.containsKey(2) = " + hashMap.containsKey(2));
+//        System.out.println("hashMap.containsKey(3) = " + hashMap.containsKey(3));
+//
+//        System.out.println("hashMap.containsValue(\"Ilya\" = )" + hashMap.containsValue("Ilya"));
+//        System.out.println("hashMap.containsValue(\"Alina\" = )" + hashMap.containsValue("Alina"));
+//        System.out.println("hashMap.containsValue(\"nothing\") = " + hashMap.containsValue("nothing"));
+//
+//        System.out.println("hashMap.size() = " + hashMap.size());
+//        System.out.println("hashMap.remove(1) = " + hashMap.remove(1));
+//        System.out.println("hashMap.remove(3) = " + hashMap.remove(3));
 
-        System.out.println("hashMap.containsKey(1) = " + hashMap.containsKey(1));
-        System.out.println("hashMap.containsKey(2) = " + hashMap.containsKey(2));
-        System.out.println("hashMap.containsKey(3) = " + hashMap.containsKey(3));
 
-        System.out.println("hashMap.containsValue(\"Ilya\" = )" + hashMap.containsValue("Ilya"));
-        System.out.println("hashMap.containsValue(\"Alina\" = )" + hashMap.containsValue("Alina"));
-        System.out.println("hashMap.containsValue(\"nothing\") = " + hashMap.containsValue("nothing"));
+        CustomHashMap<Integer, String> myMap = new CustomHashMap<>();
+        myMap.put(1, "Ilya");
+        myMap.put(1, "Alina");
+        myMap.put(2, "22");
+        myMap.put(3, "33");
+        myMap.put(4, "44");
+        myMap.put(5, "55");
+        myMap.put(6, "66");
+        myMap.put(7, "77");
+        myMap.put(8, "88");
+        myMap.put(9, "99");
+        myMap.put(10, "100");
+        System.out.println(myMap.toString());
 
-        System.out.println("hashMap.size() = " + hashMap.size());
-        System.out.println("hashMap.remove(1) = " + hashMap.remove(1));
-        System.out.println("hashMap.remove(3) = " + hashMap.remove(3));
     }
 }
